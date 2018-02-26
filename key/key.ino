ce506875f941ce3908802aff900e40154dc68351
yy710 <171254868@qq.com>
2018-02-26 17:30:13 +0800

    modify key code

--- key/key.ino
+++ key/key.ino
@@ -1,15 +1,13 @@
-/*
- *  Simple HTTP get webclient test
- */
-
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "Jingchu";
-const char* password = "zjc19800202";
+//const char* ssid     = "Jingchu";
+const char* ssid     = "dkMeet";
+//const char* password = "zjc19800202";
+const char* password = "dkbyd2018";
 
-const char* host = "112.112.5.166";
+const char* host = "www.xingshenxunjiechuxing.com";
 
-void led(int s){
+void led(int s) {
   digitalWrite(12, s);
   digitalWrite(13, s);
   //digitalWrite(14, s);
@@ -17,33 +15,25 @@ void led(int s){
   return;
 }
 
-void setup() {
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-  //pinMode(14, OUTPUT);
-  //pinMode(15, OUTPUT);
-
-  led(LOW);
+void setup()
+{
+  pinMode(12, OUTPUT);//继电器
+  pinMode(13, OUTPUT);//Blue LED
+  pinMode(14, OUTPUT);//Red LED
+  pinMode(15, OUTPUT);//Green LED
 
   Serial.begin(115200);
-  delay(100);
-
-  // We start by connecting to a WiFi network
-
   Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
+
+  Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.print("Netmask: ");
@@ -52,45 +42,47 @@ void setup() {
   Serial.println(WiFi.gatewayIP());
 }
 
-int value = 0;
-
-void loop() {
-  delay(10000);
-  ++value;
-
-  led(HIGH);
-
-  Serial.print("connecting to ");
-  Serial.println(host);
-  
-  // Use WiFiClient class to create TCP connections
+void loop()
+{
   WiFiClient client;
-  const int httpPort = 81;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    //digitalWrite(14, HIGH);
-    return;
-  }
-  
-  // We now create a URI for the request
-  String url = "/";
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-  
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  delay(500);
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("closing connection");
+  const int httpPort = 80;
+  String url = "/iot/setKey";
 
-  led(LOW);
+  Serial.printf("\n[Connecting to %s ... ", host);
+  if (client.connect(host, httpPort))
+  {
+    Serial.println("connected]");
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "\r\n"
+                );
+
+    Serial.println("[Response:]");
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+
+        if (line == String("true")) {
+          led(HIGH);
+        } else if (line == String("false")) {
+          led(LOW);
+        }
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  }
+  else
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
+  delay(5000);
 }
